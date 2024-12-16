9d29b578448faa4cf18a7582057ea523758d96df
Henry Levenberg <henrylevenberg@gmail.com>
2024-12-16 14:08:03 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <main className="max-w-md w-full bg-white rounded-lg shadow-xl overflow-hidden">
         <div className="p-6 text-center">
           <Image
-            src="https://i.ibb.co/8dBv0tf/lm-profile.jpg"
+            src="/lm/lisa-marie.jpg"
             alt="Lisa-Marie Schwingshandl"
             width={150}
             height={150}
@@ -20,7 +20,7 @@ export default function Home() {
           <p className="text-gray-600 mb-6">Business Development Partner at the CSM Family Office</p>
           
           <div className="space-y-4">
-            <Link href="/experience" passHref>
+            <Link href="/lm/experience">
               <button className="w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 inline-flex items-center justify-center">
                 <FileText className="mr-2 h-4 w-4" /> Experience
               </button>
@@ -45,3 +45,4 @@ export default function Home() {
     </div>
   )
 }
+
