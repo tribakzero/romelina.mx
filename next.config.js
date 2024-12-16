8791c60dee2cb01930ee09973bbc57d2b4ccf5d8
Henry Levenberg <henrylevenberg@gmail.com>
2024-12-16 14:11:00 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        pathname: '/**',
+      },
+    ],
   },
   basePath: '/lm',
   assetPrefix: '/lm/',
