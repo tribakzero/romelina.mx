7af6ea20600efe079a409068bae9b5942b4a6a26
Richard Garber <rg.1029384756@gmail.com>
2024-12-17 14:50:50 -0500

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Lisa-Marie Schwingshandl',
-  description: 'Business Development Partner at the CSM Family Office',
-}
+  title: "Lisa-Marie Schwingshandl",
+  description: "Business Development Partner at the CSM Family Office",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
-
