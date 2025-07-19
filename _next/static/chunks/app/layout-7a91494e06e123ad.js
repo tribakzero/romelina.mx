4ca231b8da42bfdb7f3ae2540f0e7a73b180c884
tribakzero <tribak.zero@gmail.com>
2025-07-19 09:03:56 +0000

    Deploying to gh-pages from @ tribakzero/romelina.mx@b06b41579df902c06e6cadbe72a296cc124291bf 🚀

--- _next/static/chunks/app/layout-7a91494e06e123ad.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4017:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5299,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{},5299:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_0198c8"}}},e=>{var s=s=>e(e.s=s);e.O(0,[618,441,75,358],()=>s(4017)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/page-2fbac5049cfadafa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2707:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3303,23)),Promise.resolve().then(n.t.bind(n,3584,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[303,584,441,75,358],()=>s(2707)),_N_E=e.O()}]);
