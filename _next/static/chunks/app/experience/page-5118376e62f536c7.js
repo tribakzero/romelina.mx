fa5c22dba9c4e23eb7d46f22f516715408711251
tribakzero <tribak.zero@gmail.com>
2025-07-19 08:34:11 +0000

    Deploying to gh-pages from @ tribakzero/romelina.mx@0f10928a67eb838b96a3c74722284ad2b06af597 🚀

---
+++ _next/static/chunks/app/experience/page-5118376e62f536c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[127],{4880:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3303,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[303,441,75,358],()=>s(4880)),_N_E=e.O()}]);
